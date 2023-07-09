5b30507e26bf28f05502156edca66295a3bcb063
Kirill <Kirill.pros@gmail.com>
2023-07-09 19:51:44 +0400

    Добавить валидацию данных

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const { PORT, DB_URL } = require('./config');
+const { errors } = require('celebrate');
 const router = require('./routes');
 const { handleErrors } = require('./middlewares/handleErrors');
 const { auth } = require('./middlewares/auth');
 const { login, createUser } = require('./controllers/user');
+const { validateCreateUser, validateLogin } = require('./middlewares/validation');
+const { PORT, DB_URL } = require('./config');
 
 const app = express();
 mongoose.connect(DB_URL);
 app.use(express.json());
 
-app.post('/signin', login);
-app.post('/signup', createUser);
+app.post('/signin', validateLogin, login);
+app.post('/signup', validateCreateUser, createUser);
 
 app.use(auth);
 app.use(router);
 
+app.use(errors()); // обработчик ошибок celebrate
 app.use(handleErrors); // централизованный обработчик ошибок
 
 app.listen(PORT, () => {
--- routes/movie.js
+++ routes/movie.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const { getMovie, addMovie, deleteMovie } = require('../controllers/movie');
+const { validateAddMovie, validateMovieId } = require('../middlewares/validation');
 
 router.get('/', getMovie);
-router.post('/', addMovie);
-router.post('/_id', deleteMovie);
+router.post('/', validateAddMovie, addMovie);
+router.post('/_id', validateMovieId, deleteMovie);
 
 module.exports = router;
